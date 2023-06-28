1e4984b74aa00baeac82613676e5998d123ae197
louispaulet <louispaulet13@gmail.com>
2023-06-28 02:32:08 +0200

    update depth of display

--- declaration_display.js
+++ declaration_display.js
@@ -48,20 +48,67 @@ function formatXMLData(data, depthThreshold) {
 }
 
 // Fetch XML data
-$(document).ready(function() {
+function fetchXMLData(url) {
   $.ajax({
-    url: 'https://raw.githubusercontent.com/louispaulet/hatvp_viz/main/datasets/xml_unitary_declarations/content/xml_unitary_declarations/declarations_hatvp_batch_00000001.xml',
+    url: url,
     dataType: 'xml',
     success: function(data) {
       var jsonData = xmlToJson(data);
-      var xmlHtml = formatXMLData(jsonData, 15); // Depth threshold: 3
+      var xmlHtml = formatXMLData(jsonData, 3); // Depth threshold: 3
       $('#xmlData').html(xmlHtml);
     },
     error: function() {
       $('#xmlData').html('<p>Error loading XML data.</p>');
     }
   });
+}
+
+// Populate URL dropdown
+function populateDropdown(urls) {
+  var dropdown = $('#urlDropdown');
+  dropdown.empty();
+  for (var i = 0; i < urls.length; i++) {
+    var url = urls[i];
+    dropdown.append($('<option></option>').attr('value', url).text(url));
+  }
+}
+
+// Load XML data on button click
+$('#loadButton').click(function() {
+  var selectedUrl = $('#urlDropdown').val();
+  if (selectedUrl) {
+    fetchXMLData(selectedUrl);
+  }
 });
+
+// Fetch CSV data
+$.ajax({
+  url: 'https://raw.githubusercontent.com/louispaulet/hatvp_viz/main/datasets/xml_unitary_declarations/content/unitary_dataset_url_df.csv',
+  dataType: 'text',
+  success: function(data) {
+    var urls = parseCSV(data);
+    populateDropdown(urls);
+  },
+  error: function() {
+    $('#xmlData').html('<p>Error loading URL data.</p>');
+  }
+});
+
+// Parse CSV data
+function parseCSV(csvData) {
+  var urls = [];
+  var rows = csvData.split('\n');
+  for (var i = 1; i < rows.length; i++) {
+    var row = rows[i].split(',');
+    if (row.length === 2) {
+      var url = row[1].trim();
+      if (url) {
+        urls.push(url);
+      }
+    }
+  }
+  return urls;
+}
 
 // XML to JSON conversion function
 function xmlToJson(xml) {
